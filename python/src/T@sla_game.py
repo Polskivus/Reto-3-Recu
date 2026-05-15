5f556f770216197f3a996c7938beb09067645f0b
Filip <filipiworowicz@gmail.com>
2026-05-15 13:56:46 +0200

    Prueba de monigote y prueba de salto

--- python/src/T@sla_game.py
+++ python/src/T@sla_game.py
@@ -9,7 +9,7 @@ SCREEN_WIDTH = 1400
 SCREEN_HEIGHT = 800
 
 VELOCIDAD = 300
-GRAVEDAD = 120
+GRAVEDAD = 200
 
 pantalla = pygame.display.set_mode((1400, 800), pygame.DOUBLEBUF)
 reloj = pygame.time.Clock()
@@ -19,46 +19,54 @@ dt = 0
 pantalla.fill("white")
 
 ruta_actual = os.path.dirname(__file__)
-
-print(ruta_actual)
-
 ruta_imagen_PJ = os.path.abspath(os.path.join(ruta_actual, '..', 'assets', 'img','Stick_man.png'))
-
-print(ruta_imagen_PJ)
 
 class PJ(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         try:
             self.imagen = pygame.image.load(ruta_imagen_PJ).convert_alpha()
-            print(self.imagen.get_size())
         except pygame.error as e:
             print(f"No se pudo cargar la imagen: {ruta_imagen_PJ}")
             raise SystemExit(e)
+        
+        self.saltando = False
+        self.velocidad_y = 0
+
         self.rect = self.imagen.get_rect()
         self.rect.midbottom = (pantalla.get_width() / 2, SCREEN_HEIGHT)
-        print(self.rect)
+
         self.x = float(self.rect.x)
         self.y = float(self.rect.y)
 
     def update(self, dt):
         tecla_pulsada = pygame.key.get_pressed()
 
-        if self.rect.top > 0:
-            if tecla_pulsada[pygame.K_w]:
-                self.y -= VELOCIDAD * dt
-        if self.rect.bottom < SCREEN_HEIGHT:
-            if tecla_pulsada[pygame.K_s]:
-                self.y += VELOCIDAD * dt
+        if tecla_pulsada[pygame.K_SPACE] and not self.saltando:
+            self.saltando = True
+            self.velocidad_y = -400
+
+        if self.velocidad_y > 0:
+            self.velocidad_y += GRAVEDAD *2 * dt
+        else:
+            self.velocidad_y += GRAVEDAD * dt
+
+        if self.velocidad_y < 0 and not tecla_pulsada[pygame.K_SPACE]:
+            self.velocidad_y *= 0.5
+
+        self.y += self.velocidad_y * dt
+
+        if self.y + self.rect.height >= SCREEN_HEIGHT:
+            self.y = float(SCREEN_HEIGHT - self.rect.height)
+            self.saltando = False
+            self.velocidad_y = 0
+
         if self.rect.left > 0:
             if tecla_pulsada[pygame.K_a]:
                 self.x -= VELOCIDAD * dt
         if self.rect.right < SCREEN_WIDTH:
             if tecla_pulsada[pygame.K_d]:
                 self.x += VELOCIDAD * dt
-
-        if self.rect.bottom < SCREEN_HEIGHT:
-            self.y += GRAVEDAD * dt
 
         self.rect.x = int(self.x)
         self.rect.y = int(self.y)
