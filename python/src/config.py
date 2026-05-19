e07f2b6c8dc65f4c4fe9aebeca9071ba64f2d979
Filip <filipiworowicz@gmail.com>
2026-05-19 13:29:35 +0200

    Añadido varios valores, rutas de imagenes y creada la clase de estado de juego para controlar que parte ejecutar

--- python/src/config.py
+++ python/src/config.py
@@ -1,3 +1,6 @@
+#Imports para la config
+import os
+from enum import Enum
 
 #Pantalla
 SCREEN_WIDTH = 1400
@@ -7,4 +10,23 @@ SCREEN_HEIGHT = 800
 VELOCIDAD = 300
 GRAVEDAD = 200
 
+#Colores basicos
+ROJO = (255,0,0)
+VERDE = (0,255,0)
+AZUL = (0,0,255)
+BLANCO = (255,255,255)
+CASIMARRON = (225, 193, 110)
+
+#Fotogramas por segundo
 FPS = 60
+
+#Rutas para las imagenes
+ruta_actual = os.path.dirname(__file__)
+ruta_imagen_PJ = os.path.abspath(os.path.join(ruta_actual, '..', 'assets', 'img','Stick_man.png'))
+ruta_imagen_PJ_salto = os.path.abspath(os.path.join(ruta_actual, '..', 'assets', 'img','Saltando.png'))
+
+#Posibles estados de juego
+class estadoJuego(Enum):
+    SALIR = -1
+    PRINCIPAL = 0
+    NUEVO_JUEGO = 1
