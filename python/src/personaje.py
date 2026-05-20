a16b2ea99ca2160a37e5c81fea9cfc5a43729f21
Filip <filipiworowicz@gmail.com>
2026-05-20 10:51:03 +0200

    Cambiado el orden del update, para que se cree primero la imagen luego se compruebe la colision, tambien se ha puesto un rect en la cabeza del personaje, antes esta echo de manera que si el PJ chocaba con el bloque por el lado por ejemplo, se empujaba al personaje debajo del bloque, no queria eso, ahora con el rect en su cabeza solo lo hace ir abajo si choca con ese punto

--- python/src/personaje.py
+++ python/src/personaje.py
@@ -35,9 +35,9 @@ class PJ(pygame.sprite.Sprite):
     def update(self, dt, bloques):
         self.control_input(dt)
         self.aplicar_gravedad(dt)
-        self.check_colisiones(bloques)
         self.actualizar_img()
-    
+        self.check_colisiones(bloques)
+
     def control_input(self, dt):
         tecla_pulsada = pygame.key.get_pressed()
 
@@ -50,7 +50,7 @@ class PJ(pygame.sprite.Sprite):
         if tecla_pulsada[K_SPACE] and not self.saltando and self.en_suelo:
             self.saltando = True
             self.velocidad_y = -400
-    
+
     def aplicar_gravedad(self, dt):
         if not self.en_suelo:
             tecla_pulsada = pygame.key.get_pressed()
@@ -71,15 +71,16 @@ class PJ(pygame.sprite.Sprite):
 
     def check_colisiones(self, bloques):
         self.rect.x = int(self.x)
-        for bloque in pygame.sprite.spritecollide(self, bloques, False, pygame.sprite.collide_mask):
+        for bloque in pygame.sprite.spritecollide(self, bloques, False):
             if self.rect.centerx < bloque.rect.centerx:
                 self.rect.right = bloque.rect.left
             else:
                 self.rect.left = bloque.rect.right
             self.x = float(self.rect.x)
-        
+
         self.rect.y = int(self.y)
         rect_pies = pygame.Rect(self.rect.x, self.rect.bottom - 2, self.rect.width, 4)
+        rect_cabeza = pygame.Rect(self.rect.x, self.rect.top, self.rect.width, 4)
         self.en_suelo = rect_pies.bottom >= SCREEN_HEIGHT
 
         for bloque in bloques:
@@ -89,7 +90,7 @@ class PJ(pygame.sprite.Sprite):
                 self.saltando = False
                 self.velocidad_y = 0
                 self.en_suelo = True
-            elif self.rect.colliderect(bloque.rect) and self.velocidad_y < 0:
+            elif rect_cabeza.colliderect(bloque.rect) and self.velocidad_y < 0:
                 self.rect.top = bloque.rect.bottom
                 self.y = float(self.rect.y)
                 self.velocidad_y = 0
@@ -102,18 +103,21 @@ class PJ(pygame.sprite.Sprite):
     def mostrar_hitbox(self, surface):
         pygame.draw.rect(surface, (0,0,0), self.rect, 2)
 
+        rect_cabeza = pygame.Rect(self.rect.x, self.rect.top, self.rect.width, 4)
+        pygame.draw.rect(surface, (0,0,0), rect_cabeza, 2)
+
         rect_pies = pygame.Rect(self.rect.x, self.rect.bottom - 2, self.rect.width, 4)
         pygame.draw.rect(surface, (0,0,0), rect_pies, 2)
 
 
 """
 
-PRIMERA VERSION ESTA TO-DO METIDO EN UPDATE ME PARECIA FEO ASI QUE HE CAMBIADO PARA QUE 
+PRIMERA VERSION ESTA TO-DO METIDO EN UPDATE ME PARECIA FEO ASI QUE HE CAMBIADO PARA QUE
 CADA COSA VAYA INDIVIDUALMENTE Y EL UPDATE LAS RECOGA PARA EJECUTAR
 
     def update(self, dt, bloques):
         tecla_pulsada = pygame.key.get_pressed()
-        
+
         rect_pies = pygame.Rect(self.rect.x, self.rect.bottom, self.rect.width, 2)
         self.en_suelo = rect_pies.bottom >= SCREEN_HEIGHT or any(
             rect_pies.colliderect(b.rect) for b in bloques
@@ -151,7 +155,7 @@ CADA COSA VAYA INDIVIDUALMENTE Y EL UPDATE LAS RECOGA PARA EJECUTAR
 
         imagen_base = self.imagen_salto if self.saltando else self.imagen_normal
         self.imagen = pygame.transform.flip(imagen_base, self.direccion == 0, False)
-        
+
         self.rect.x = int(self.x)
 
         colisiones = pygame.sprite.spritecollide(self, bloques, False)
